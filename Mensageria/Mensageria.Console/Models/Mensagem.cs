ab41300f764a9b50d655fc0af8a6be61006a4aa3
zehpavora <zehpavora@gmail.com>
2022-02-17 20:06:35 -0300

    Definição básica do modelo

---
+++ Mensageria/Mensageria.Console/Models/Mensagem.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+
+namespace Mensageria.Console.Models;
+
+public class Mensagem : IEquatable<Mensagem?>, IComparable<Mensagem>
+{
+    #region Properties
+    public string Conteudo { get; set; }
+
+    public DateTimeOffset DataHoraEnvio { get; set; }
+
+    public IPAddress IPAddress { get; set; }
+    #endregion
+
+    #region Equality
+    /// <inheritdoc/>
+    public override bool Equals(object? obj)
+    {
+        return this.Equals(obj as Mensagem);
+    }
+
+    /// <inheritdoc/>
+    public bool Equals(Mensagem? other)
+    {
+        return other is not null
+            && this.DataHoraEnvio.Equals(other.DataHoraEnvio)
+            && EqualityComparer<IPAddress>.Default.Equals(this.IPAddress, other.IPAddress);
+    }
+
+    /// <inheritdoc/>
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(this.DataHoraEnvio, this.IPAddress);
+    }
+    #endregion
+
+    /// <inheritdoc/>
+    public int CompareTo(Mensagem? other)
+    {
+        return other is not null
+             ? this.DataHoraEnvio.CompareTo(other.DataHoraEnvio)
+             : 1;
+    }
+}
